678bfa51214aa95cf31e7bde4336893f3a6b438a
Zoe <mitchellzh101@gmail.com>
2020-06-01 12:43:42 -0500

    adding pull requests

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -16,22 +16,22 @@
  * Adds a random greeting to the page.
  */
 function addRandomGreeting() {
-  const greetings =
+  var greetings =
       ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
   // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  var greeting = greetings[Math.floor(Math.random() * greetings.length)];
 
   // Add it to the page.
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
 function addRandomIntro() {
-  const intros =
+  var intros =
       ['Student', 'Developer', 'Woman', 'Human'];
 
   // Pick a random greeting.
-  const intro = intros[Math.floor(Math.random() * intros.length)];
+  var intro = intros[Math.floor(Math.random() * intros.length)];
 
   // Add it to the page.
   const introContainer = document.getElementById('intro-container');
