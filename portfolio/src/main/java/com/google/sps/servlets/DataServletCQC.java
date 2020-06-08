1b3ad1f978cf98747583dd741ce30a1cb21ca602
Zoe <mitchellzh101@gmail.com>
2020-06-08 18:06:08 +0000

    import order sorting

--- portfolio/src/main/java/com/google/sps/servlets/DataServletCQC.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServletCQC.java
@@ -14,14 +14,15 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
+
 
 @WebServlet("/my-CQCs")
 public class DataServletCQC extends HttpServlet { //Data Servlet for Comment Question Concern form in portfolio
