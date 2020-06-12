40c781887aabd7cbe74cde3d55d03030f2d91b9d
Zoe <mitchellzh101@gmail.com>
2020-06-12 15:51:41 +0000

    adding a map of chicago to my website

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -101,3 +101,11 @@ window.onload = function () {
     }
   };
 };
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 41.882141, lng: -87.652206 },
+    zoom: 8
+  });
+}
